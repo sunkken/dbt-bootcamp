cb9d6de1a959de53f8468e420226723689227157
sunkken <34925100+sunkken@users.noreply.github.com>
2026-01-13 18:32:13 +0200

    add Section 5: Seeds and Sources

--- airbnb/models/src/src_hosts.sql
+++ airbnb/models/src/src_hosts.sql
@@ -1,7 +1,7 @@
 WITH raw_hosts AS (
   SELECT *
   FROM
-    AIRBNB.RAW.RAW_HOSTS
+    {{ source('airbnb', 'hosts') }}
 )
 SELECT
   id AS host_id,
--- airbnb/models/src/src_listings.sql
+++ airbnb/models/src/src_listings.sql
@@ -1,7 +1,7 @@
 WITH raw_listings AS (
   SELECT *
   FROM
-    AIRBNB.RAW.RAW_LISTINGS
+    {{ source('airbnb', 'listings') }}
 )
 SELECT
   id AS listing_id,
