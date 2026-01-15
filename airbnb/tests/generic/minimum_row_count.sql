ea92c144751608924805151403e741e02bdca818
sunkken <34925100+sunkken@users.noreply.github.com>
2026-01-15 12:10:11 +0200

    add Section 8: Advanced Testing

---
+++ airbnb/tests/generic/minimum_row_count.sql
@@ -0,0 +1,7 @@
+{% test minimum_row_count(model, min_row_count) %}
+{{ config(severity = 'warn') }}
+    SELECT
+      COUNT(*) AS row_count
+    FROM {{ model }}
+    HAVING COUNT(*) < {{ min_row_count }}
+{% endtest %}
