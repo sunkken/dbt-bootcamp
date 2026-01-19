3f7958c15a864c2c81c6e949ae901b12a0e33e32
sunkken <34925100+sunkken@users.noreply.github.com>
2026-01-19 14:51:18 +0200

    updated folder structure and initialized dagster project; dbt project is now inside transform/ folder and dagster project inside orchestration/ folder

---
+++ orchestration/orchestration/project.py
@@ -0,0 +1,11 @@
+import os
+from pathlib import Path
+
+from dagster_dbt import DbtProject
+
+airbnb_project = DbtProject(
+    project_dir=Path(__file__).joinpath("..", "..", "..", "transform").resolve(),
+    packaged_project_dir=Path(__file__).joinpath("..", "..", "dbt-project").resolve(),
+    profiles_dir=Path(os.path.expanduser("~")).joinpath(".dbt").resolve(),
+)
+airbnb_project.prepare_if_dev()
